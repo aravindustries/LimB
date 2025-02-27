7533ba4a7bde3f6c1315adadb2cc9b8642cdf169
aGeb1 <arigebhardt82@gmail.com>
2025-02-27 17:44:18 -0500

    small change

--- simulation/cnn.py
+++ simulation/cnn.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 
