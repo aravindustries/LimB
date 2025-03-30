a9840cf51c7c228b1c74ca80a109bb8d0deac36a
aGeb1 <arigebhardt82@gmail.com>
2025-03-30 00:27:02 -0400

    adjusted agile

--- simulation/compare2.py
+++ simulation/compare2.py
@@ -7,7 +7,7 @@ import pandas as pd
 from agile_testbed import Agile
 
 
-B = 16
+B = 4
 angles = np.hstack([[0], np.linspace(-45, 45, 62)])
 indexes = np.round(np.linspace(11, 52, B)).astype(int)
 agile = Agile(angles[indexes])
@@ -16,7 +16,7 @@ for path in ['low_snr', 'mid_high_snr', 'mid_low_snr', 'mid_snr', 'ultra_low_snr
     folder = os.path.join("../data/spacial_profiles", path)
     combined_df = pd.DataFrame()
 
-    y_hat = np.zeros(91)
+    y_err = np.zeros(91)
     for file in os.listdir(folder):
         fp = os.path.join(folder, file)
         df = pd.read_csv(fp)
@@ -26,12 +26,12 @@ for path in ['low_snr', 'mid_high_snr', 'mid_low_snr', 'mid_snr', 'ultra_low_snr
 
         for i in range(X.shape[0] // 91):
             X_sub = X[i*91:(i+1)*91]
-            y_hat += agile.call(X_sub)
+            y_err += np.abs(agile.call(X_sub) - np.arange(-45, 46))
 
-        y_hat /= X.shape[0] // 91
+        y_err /= X.shape[0] // 91
 
-    err = np.abs(y_hat - np.arange(-45, 46))
-    plt.plot(np.arange(-45, 46), err, label=path)
+    # err = np.abs(y_hat - np.arange(-45, 46))
+    plt.plot(np.arange(-45, 46), y_err, label=path)
 
 # plt.plot(np.arange(-45, 46), np.arange(-45, 46), 'k--', label="True Angle")
 plt.xlabel("True Angle (°)")
